79ed1262d00efaa95b1b903549ff87fcab9b573e
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-19 02:15:17 +0200

    fix: sort dependency graph exports

--- src/dependencyGraph.ts
+++ src/dependencyGraph.ts
@@ -1,5 +1,5 @@
-export { kotlinDependencyGraphAnalyzer } from './features/dependency-analysis/adapters/outbound/kotlin/kotlinDependencyGraphAnalyzer.js';
 export { javaDependencyGraphAnalyzer } from './features/dependency-analysis/adapters/outbound/java/javaDependencyGraphAnalyzer.js';
+export { kotlinDependencyGraphAnalyzer } from './features/dependency-analysis/adapters/outbound/kotlin/kotlinDependencyGraphAnalyzer.js';
 export { typescriptDependencyGraphAnalyzer } from './features/dependency-analysis/adapters/outbound/typescript/typescriptDependencyGraphAnalyzer.js';
 export { createDependencyGraphAsync } from './features/dependency-analysis/application/createDependencyGraphAsync.js';
 export type {
